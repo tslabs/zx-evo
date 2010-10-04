6cced87b3dcbae09d53443b2b5d3a3c6d509a034
at2313 <at2313@localhost>
2010-10-04 20:23:37 +0000

    SounDrive (#0F,#1F,#4F,#5F) and Covox (#FB|#DD), stable

--- src/fpga/current/sound/covox.v
+++ src/fpga/current/sound/covox.v
@@ -6,44 +6,35 @@
 // PWM parameters: 27,34375 kHz(28000/1024) 10 bit
 
 module covox(
-
 	input fclk,  // global FPGA clock, 28MHz
-	input [7:0] psd0, psd1, psd2, psd3
-
-	output beep,
-	
-	reg [9:0] saw;
-	
-	wire [9:0] ppwm;
-
+	input [7:0] psd0, psd1, psd2, psd3,
+	output reg beep
 	);
+
+	reg [9:0] saw;
+	reg [9:0] pwm;
 
 initial
 begin
 	saw = 10'd0;
-end
-	
-//The resulting PWM is the sum of 4 SD channels
-always @*
-begin
-	ppwm <= psd0[7:0] + psd1[7:0] + psd2[7:0] + psd3[7:0]
+	pwm = 0;
 end
 	
 //'Saw' counter for PWM 0..1023
-always @(posedge clk)
+always @(posedge fclk)
 begin
-	saw <= saw + 10'd1;
+	saw <= saw + 10'd1;	//inc saw
+
+	if (saw == pwm)
+	beep <= 1'b0;	//BEEP is reset on the end of PWM duty cycle
+	else if (saw == 10'b0)
+	beep <= 1'b1;	//BEEP is set on the begin of PWM duty cycle
+end
+
+//Update PWM with the sum of 4 SD channels
+always @(negedge saw[9])
+begin
+	pwm <= (psd0 + psd1 + psd2 + psd3);
 end
 		
-always @*
-begin
-//BEEP is set on the begin of PWM duty cycle
-	if (saw==(10'b0))
-	beep <= 1'b1;
-
-//BEEP is reset on the end of PWM duty cycle
-	if (saw==ppwm[9:0])
-	beep <= 1'b0;
-end
-
 endmodule
