f32f543c874b7d0aee9f490a35f1aa1793d4e67c
at2313 <at2313@localhost>
2010-10-27 21:06:07 +0000

    Stub for SPU state-machine (SM)

--- src/fpga/current/video/sprites.v
+++ src/fpga/current/video/sprites.v
@@ -9,9 +9,9 @@
 //
 // TV Line Cycles - 448:
 // Visible Area  - 360 * 288 pixels:
-// 52 - border
-// 256 - pixels
-// 52 - border
+// 		52 - border
+// 		256 - pixels
+// 		52 - border
 
 
 module sprites(
@@ -20,9 +20,8 @@ module sprites(
 	input pre_vline, s_reload,
 	input [7:0] din,
 	
-	output [7:0] sf_ra,
+	output reg [7:0] sf_ra, sp_ra,
 	input [7:0] sf_rd,
-	output [7:0] sp_ra,
 	input [5:0] sp_rd,
 	
 	output reg [5:0] spixel,
@@ -31,12 +30,7 @@ module sprites(
 	);
 
 	reg [8:0] vline;
-	reg l_sel;
-	
-	initial
-	begin
-//	sl_ra = 9'b111111111;
-	end
+	reg l_sel, sl_ws;
 	
 /*
 	initial 
@@ -74,58 +68,111 @@ module sprites(
 	end
 	
 	
-	//processing sprite array
-	parameter SpEn = 3'd0;
-	parameter SpCR = 3'd0;
-	parameter SpPal = 3'd0;
-	parameter SpLoAddr = 3'd1;
-	parameter SpMiAddr = 3'd2;
-	parameter SpHiAddr = 3'd3;
-	parameter SpLoX = 3'd4;
-	parameter SpHiX = 3'd5;
-	parameter SpXSz = 3'd5;
-	parameter SpLoY = 3'd6;
-	parameter SpHiY = 3'd7;
-	parameter SpYSz = 3'd7;
-	
-	
-	
+	// processing of the sprite array
+		
 	localparam VLINES = 9'd288;
 	
 	reg [5:0] sp_num;
-	reg sp_word_rdy;
-	reg [2:0] sp_mc;
+	reg [7:0] sp_mc;
+	reg sac_ws, san_ws;
+	reg [7:0] sac_wd;
+	reg [13:0] sa_cnt;
+
+// sfile
+	reg sp_act;
+	reg sp_cres;
+	reg [5:0] sp_pal;
+	reg [20:0] sp_addr;		//word address!!! 2MB x 16bit
+	reg [8:0] sp_xpos;
+	reg [8:0] sp_ypos;
+	reg [6:0] sp_xsz;
+	reg [6:0] sp_ysz;
+
 	
+// Marlezonskiy balet, part I
+// *** DIE MASCHINE *** !!!
+
 	always @(posedge clk, posedge line_start)
 	if (line_start)
-	begin
-		sp_num <= 6'd0;
-		sp_word_rdy <= 1'b0;
-		sp_mc <= 3'b0;
-	
-	
-	
-	
-	
-	
-	end
+		sp_mc <= 8'b0;
 	else
+	
 	begin
-	
-	
-	
-	
+	case (sp_mc)
+
+	0:	//reset SPU, set sprite number to 0
+	begin
 		sp_num <= 6'd0;
-		sp_word_rdy <= 1'b0;
-		sp_mc <= 3'b0;
-	
-	
-	
-	
 	end
+
+	1:	//set addr for SPReg0
+	begin
+		sf_ra <= {sp_num[4:0], 3'd0};
+	end
+
+	2: //read SPReg0, set addr for SPReg1
+	begin
+		sp_act <= sf_rd[7];
+		sp_cres <= sf_rd[6];
+		sp_pal <= sf_rd[5:0];
+		sf_ra <= {sp_num[4:0], 3'd1};
+	end
+
+	3: //read SPReg1, set addr for SPReg2
+	begin
+		sp_addr[7:0] <= sf_rd[7:0];
+		sf_ra <= {sp_num[4:0], 3'd2};
+	end
+
+	4: //read SPReg2, set addr for SPReg3
+	begin
+		sp_addr[15:8] <= sf_rd[7:0];
+		sf_ra <= {sp_num[4:0], 3'd3};
+	end
+
+	5: //read SPReg3, set addr for SPReg4
+	begin
+		sp_addr[20:16] <= sf_rd[4:0];
+		sf_ra <= {sp_num[4:0], 3'd4};
+	end
+
+	6: //read SPReg4, set addr for SPReg5
+	begin
+		sp_xpos[7:0] <= sf_rd[7:0];
+		sf_ra <= {sp_num[4:0], 3'd5};
+	end
+
+	7: //read SPReg5, set addr for SPReg6
+	begin
+		sp_xpos[8] <= sf_rd[0];
+		sp_xsz[6:0] <= sf_rd[7:1];
+		sf_ra <= {sp_num[4:0], 3'd6};
+	end
+
+	8: //read SPReg6, set addr for SPReg7
+	begin
+		sp_ypos[7:0] <= sf_rd[7:0];
+		sf_ra <= {sp_num[4:0], 3'd7};
+	end
+
+	9: //read SPReg7, reset sa_cnt to 0
+	begin
+		sp_ypos[8] <= sf_rd[0];
+		sp_ysz[6:0] <= sf_rd[7:1];
+	end
+
+
 	
 	
+//	default:		//halt
+	endcase
 	
+	if (!sp_mc[7])
+	begin
+		sp_mc <= sp_mc + 8'b1;
+	end
+	end
+/*	
 	
 	//nulling of sprites address counters
 	reg san_ws;
@@ -159,29 +206,7 @@ module sprites(
 	
 	
 	
-	//write to sa_cnt
-	always @(posedge sac_ws, posedge san_ws)
-	begin
-		if (sac_ws)
-		begin
-			sa_ws <= sac_ws;
-			sa_wa <= sac_wa[4:0];
-			sa_wd <= sac_wd;
-		end	
-		else if (san_ws)
-		begin
-			sa_ws <= san_ws;
-			sa_wa <= san_cnt[4:0];
-			sa_wd <= 14'b0;
-		end
-		else
-		begin
-			sa_ws <= 1'b1;
-		end
-	end
 
-	
-	/*
 	//1/25 frame color booster
 	reg fld;
 	always @(posedge vblank)
@@ -191,17 +216,12 @@ module sprites(
 	*/
 	
 	
-	//Marlesonian ballet - part I
-	
+// Marlezonskiy balet, part II	
 
-	
-	
+//read from sline
 	always @(posedge clk)
-	if (!spr_en)
-
-	begin
-	
-	//read from sline
+//	if (!spr_en)
+//	begin
 	case (l_sel)
 	0:
 	begin
@@ -214,29 +234,60 @@ module sprites(
 		spx_en <= sl_rd1[6];
 	end
 	endcase
+//	end	
+
 	
-	
-	//write to sline
+//write to sline
+	always @(posedge sl_ws)
 	case (l_sel)
 	0:
 	begin
-		sl_ws1 <= clk;
+		sl_ws1 <= sl_ws;
 		sl_ws0 <= 1'b0;
 	end
 	1:
 	begin
-		sl_ws0 <= clk;
+		sl_ws0 <= sl_ws;
 		sl_ws1 <= 1'b0;
 	end
 	endcase
 
-	if (cend)
+//write to sacnt
+	always @(posedge sac_ws, posedge san_ws)
 	begin
-		sl_wa <= sl_ra;
+		if (sac_ws)
+		begin
+			sa_ws <= sac_ws;
+			sa_wa <= sp_num[4:0];
+			sa_wd <= sa_cnt;
+		end	
+		else if (san_ws)
+		begin
+			sa_ws <= san_ws;
+			sa_wa <= sp_num[4:0];
+			sa_wd <= 14'b0;
+		end
+		else
+		begin
+			sa_ws <= 1'b1;
+		end
 	end
+
 	
-	//sprites file reload
+	
+	//	if (cend)
+//	begin
+//		sl_wa <= sl_ra;
+//	end
+
+//	end
+
+//	else spx_en <= 1'b0;
+
+
+
 /*
+	//sprites file reload
 		if  (s_reload)
 		begin
 //			spx_en <= 1;
@@ -253,9 +304,7 @@ module sprites(
 		else
 			spx_en <= 0;
 */
-			end
-	
-//	else spx_en <= 1'b0;
+			
 
 
 
