bf9af039e66029e3824c244a075aee3cd7f39511
at2313 <at2313@localhost>
2010-11-04 21:25:54 +0000

    Added C source

--- src/demos/bobbles.c
+++ src/demos/bobbles.c
@@ -1,30 +1,20 @@
 
-const char data[] = {0xff,0xaa};
-void out_xt (unsigned char, unsigned char);
+#include "ball.h"
 
-void main()
+main()
 {
 	unsigned int i = 0;
+	unsigned char d;
 
-	while(i<=7)
+	while(i<=1024)
 	{
-//		if( data[i] == 0 ) break;
-		out_xt(i, data[i]);
+		d=ball[i];
 		i++;
-
 	}
 }
 
 	
- void out_xt (unsigned char r, unsigned char v)
 
- {	__asm
-	ld bc,#0x55ff
-	ld e,#0xaa
-	out (c),e
-	ret
-	__endasm;
-}
 	
 
 
