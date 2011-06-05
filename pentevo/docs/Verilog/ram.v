f9df5f1986afbf9a6b2dbbaf9e9f0488fcf09c25
at2313 <at2313@localhost>
2011-06-05 18:45:01 +0000

    Memory modules added, APU stub, doc updated

---
+++ pentevo/docs/Verilog/ram.v
@@ -0,0 +1,28 @@
+//syncro RAM model with rdena
+//will be put into LE's - for synthesis ONLY!!!
+	always @(posedge clk)
+	if (rdena)
+		rddata_r <= mem[rdaddr];
+	assign rddata = rddata_r;
+
+	always @(posedge clk)
+	if (wrena)
+		mem[wraddr] <= wrdata;
+
+
+//syncro RAM model w/o rdena
+	always @(posedge clk)
+		rddata <= mem[rdaddr];
+	
+	always @(posedge clk)
+	if (wrena)
+		mem[wraddr] <= wrdata;
+
+		
+//asyncro out RAM model
+	always @*
+		rddata <= mem[rdaddr];
+	
+	always @(posedge clk)
+	if (wrena)
+		mem[wraddr] <= wrdata;
