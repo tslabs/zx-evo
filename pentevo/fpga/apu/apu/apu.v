91c4d4c51a08c69d153e06180e0046974d26d73b
at2313 <at2313@localhost>
2011-06-19 22:50:53 +0000

    temp

--- pentevo/fpga/apu/apu/apu.v
+++ pentevo/fpga/apu/apu/apu.v
@@ -47,86 +47,259 @@ module apu(
 	
 	);
 
-
-	reg _c, _z, _n;			//flags
+	wire sram_data_r = 16'd0;
 
 	
-//- Instructions and operands decoding ------------------------
-	localparam ld_imm	= 4'h0;
-	localparam and_imm	= 4'h1;
-	localparam or_imm	= 4'h2;
-	localparam xor_imm	= 4'h3;
-	localparam add_imm	= 4'h4;
-	localparam sub_imm	= 4'h5;
-	localparam log1		= 4'h6;
-	localparam log2		= 4'h7;
-	localparam wt_pin	= 4'h8;
-	localparam o_pin	= 4'h9;
-	localparam wt_port	= 4'hA;
-	localparam io_port	= 4'hB;
-	localparam out_imm	= 4'hC;
-	localparam jr		= 4'hD;
-	localparam djnz		= 4'hE;
-	localparam misc		= 4'hF;
+// to do
+//
+// * jc
+// * wc
+// * pins in
+// pins out
+// * port in
+// port out
+// flags
+// instructions
+// ALU
+// * GRP sz
+// port_r_sz
 
-	localparam m_log	= 4'h0;
-	localparam m_shft1	= 4'h1;
-	localparam m_shft2	= 4'h2;
-//	localparam xor_imm	= 4'h3;
-//	localparam add_imm	= 4'h4;
-//	localparam sub_imm	= 4'h5;
-//	localparam log1		= 4'h6;
-//	localparam log2		= 4'h7;
-	localparam m_bit	= 4'h8;
-	localparam m_o_pin	= 4'h9;
-//	localparam wt_port	= 4'hA;
-//	localparam port_io	= 4'hB;
-//	localparam out_imm	= 4'hC;
-	localparam m_jp		= 4'hD;
-	localparam m_mul	= 4'hE;
-	localparam m_misc	= 4'hF;
 
-	localparam s8 	= 2'b00;
-	localparam s16 	= 2'b01;
-	localparam s24 	= 2'b10;
-	localparam s32 	= 2'b11;
+//- Operands decoding -----------------------------------------
 
-	wire [3:0] ih = instr[15:12];	// instruction class
-	wire [3:0] im = instr[11:8];	// instruction sub-class
+	wire [3:0]	ih		= instr[15:12];					// instruction class 4
+	wire [3:0]	im		= instr[11:8];					// instruction sub-class 4
+
+	wire [2:0]	a1fh		= instr[14:12];				// ALU1 function class 4
+	wire [2:0]	a1fm		= {instr[12], instr[7:6]};	// ALU1 function sub-class 4
+	wire [1:0]	a2fm		= {instr[7:6]};				// ALU2 function sub-class 4
+	wire [1:0]	a3fm		= {instr[5:4]};				// ALU3 function sub-class 4
+	wire [2:0]	a4fm		= {instr[8:6]};				// ALU4 function sub-class 4
+						
+	wire [7:0]	imm		= instr[7:0];					// immediate 8
+
+	wire [15:0]	rel		= {{8{instr[7]}}, instr[7:0]};	// relative addr 16
 	
-	wire [7:0] imm = instr[7:0];	// immediate 8
-	wire [4:0] pin0 = instr[4:0];	// pin0 5
-	wire [4:0] pin1 = instr[9:5];	// pin1 5
-	wire [3:0] src = instr[3:0];	// source reg8 4
-	wire [3:0] src16 = {instr[3:1], 1'b0};		// source reg16 4
-	wire [3:0] src32 = {instr[3:2], 2'b0};		// source reg32 4
-	wire [3:0] dst = instr[11:8];	// destination reg8 4
-	wire [3:0] dst16 = {instr[11:9], 1'b0};		// destination reg16 4
-	wire [3:0] dst32 = {instr[11:10], 2'b0};	// destination reg32 4
-	wire [3:0] port = instr[3:0];	// source port8 4
-	wire [3:0] jc = instr[11:8];	// jump condition 4
-	wire [2:0] jc_m = instr[7:4];	// jump condition in miscs 4
-	wire [2:0] bit = instr[6:4];	// bit select 3
-	wire [1:0] sz = instr[5:4];		// sizeness 2
-	wire [1:0] wc = instr[7:6];		// wait condition 2
-	wire bit0 = instr[10];			// bit0 value
-	wire bit1 = instr[11];			// bit1 value
-	wire cond = instr[11];			// condition
-	wire mask = instr[10];			// mask
-	wire flag = instr[7];			// flag select
+	wire [3:0]	src8	= instr[3:0];					// source reg8 4
+	wire [3:0]	src16	= {instr[3:1], 1'b0};			// source reg16 4
+	wire [3:0]	src32	= {instr[3:2], 2'b0};			// source reg32 4
 
-	reg [15:0] rel;
+	wire [3:0]	dst8	= instr[11:8];					// destination reg8 4
+	wire [3:0]	dst16	= {instr[11:9], 1'b0};			// destination reg16 4
+	wire [3:0]	dst32	= {instr[11:10], 2'b0};			// destination reg32 4
+
+	wire [1:0]	sz		= instr[5:4];					// sizeness 2
+
+	wire [3:0]	jc		= instr[11:8];					// jump condition 4
+	wire [1:0]	wc		= instr[7:6];					// wait condition 2
+	wire 		ic		= instr[11];					// inverse condition 1
+
+	wire [4:0]	pin0	= instr[4:0];					// pin0 5
+	wire [4:0]	pin1	= instr[9:5];					// pin1 5
+	wire [1:0]	stb		= instr[7:6];					// strobe signal 2
+
+
+//- Instructions decoding -------------------------------------
+
+//- ALU arguments decoding ------------------------------------
+	wire [31:0] ih_arg1[0:15];		// first argument for main class
+	wire [31:0] ih_arg2[0:15];		// second argument for main class
 	
-	always @*
-	case (ih)		// synthesis full_case 
+	assign ih_arg1[4'b0000] = 32'hXXXX;
+	assign ih_arg1[4'b0001] = gpr_dst_sz[sz];
+	assign ih_arg1[4'b0010] = gpr_dst_sz[sz];
+	assign ih_arg1[4'b0011] = gpr_dst_sz[sz];
+	assign ih_arg1[4'b0100] = gpr_dst_sz[sz];
+	assign ih_arg1[4'b0101] = gpr_dst_sz[sz];
+	assign ih_arg1[4'b0110] = gpr_dst_sz[sz];
+	assign ih_arg1[4'b0111] = gpr_dst_sz[sz];
+	assign ih_arg1[4'b1000] = gpr_dst_sz[sz];
+	assign ih_arg1[4'b1001] = gpr_dst_sz[sz];
+	assign ih_arg1[4'b1010] = 32'hXXXX;
+	assign ih_arg1[4'b1011] = port_r_sz[sz];
+	assign ih_arg1[4'b1100] = 32'hXXXX;
+	assign ih_arg1[4'b1101] = 32'hXXXX;
+	assign ih_arg1[4'b1110] = gpr_dst_sz[sz];
+	assign ih_arg1[4'b1111] = 32'hXXXX;
 
-jr:		// jr jc, rel9
-		rel = {8{instr[7]}, instr[7:0]};
+	assign ih_arg2[4'b0000] = {24'b0, imm};
+	assign ih_arg2[4'b0001] = {24'b0, imm};
+	assign ih_arg2[4'b0010] = {24'b0, imm};
+	assign ih_arg2[4'b0011] = {24'b0, imm};
+	assign ih_arg2[4'b0100] = {24'b0, imm};
+	assign ih_arg2[4'b0101] = {24'b0, imm};
+	assign ih_arg2[4'b0110] = {24'b0, imm};
+	assign ih_arg2[4'b0111] = {24'b0, imm};
+	assign ih_arg2[4'b1000] = gpr_src_sz[sz];
+	assign ih_arg2[4'b1001] = gpr_src_sz[sz];
+	assign ih_arg2[4'b1010] = 32'hXXXX;
+	assign ih_arg2[4'b1011] = gpr_src_sz[sz];
+	assign ih_arg2[4'b1100] = 32'hXXXX;
+	assign ih_arg2[4'b1101] = 32'hXXXX;
+	assign ih_arg2[4'b1110] = gpr_dst_sz[sz];
+	assign ih_arg2[4'b1111] = gpr_src_sz[sz];
 
-djnz:	// djnz src, rel8
-		rel = {8{instr[11]}, instr[11:4]};
 
-	endcase
+//- GPR Sizeness decoding -------------------------------------
+	wire [31:0] gpr_src_sz[0:3];
+	
+	assign gpr_src_sz[2'b00] = {24'b0, gpr[src8]};
+	assign gpr_src_sz[2'b01] = {16'b0, gpr[src16+1], gpr[src16]};
+	assign gpr_src_sz[2'b10] = {8'b0, gpr[src32+2], gpr[src32+1], gpr[src32]};
+	assign gpr_src_sz[2'b11] = {gpr[src32+3], gpr[src32+2], gpr[src32+1], gpr[src32]};
+
+
+	wire [31:0] gpr_dst_sz[0:3];
+	
+	assign gpr_dst_sz[2'b00] = {24'b0, gpr[dst8]};
+	assign gpr_dst_sz[2'b01] = {16'b0, gpr[dst16+1], gpr[dst16]};
+	assign gpr_dst_sz[2'b10] = {8'b0, gpr[dst32+2], gpr[dst32+1], gpr[dst32]};
+	assign gpr_dst_sz[2'b11] = {gpr[dst32+3], gpr[dst32+2], gpr[dst32+1], gpr[dst32]};
+
+
+//- ALU1 fuctions ---------------------------------------------
+	wire [31:0] afunc1[0:7];
+
+	assign afunc1[3'b000] = ih_arg2[ih];					// ld
+	assign afunc1[3'b001] = ih_arg1[ih] & ih_arg2[ih];		// and
+	assign afunc1[3'b010] = ih_arg1[ih] | ih_arg2[ih];		// or
+	assign afunc1[3'b011] = ih_arg1[ih] ^ ih_arg2[ih];		// xor
+	assign afunc1[3'b100] = ih_arg1[ih] + ih_arg2[ih];		// add
+	assign afunc1[3'b101] = ih_arg1[ih] - ih_arg2[ih];		// sub
+	assign afunc1[3'b110] = ih_arg1[ih] - ih_arg2[ih];		// cmp
+	assign afunc1[3'b111] = ih_arg1[ih] & ih_arg2[ih];		// tst
+
+	
+//- ALU2 fuctions ---------------------------------------------
+	wire [31:0] afunc2[0:3];
+
+	assign afunc2[2'b00] = ih_arg2[ih] + 32'b1;				// inc
+	assign afunc2[2'b01] = ih_arg2[ih] - 32'b1;				// dec
+	assign afunc2[2'b10] = ih_arg2[ih] ^ 32'hFFFF;			// cpl
+	assign afunc2[2'b11] = - ih_arg2[ih];					// neg
+
+	
+//- ALU3 fuctions ---------------------------------------------
+	wire [7:0] afunc3[0:3];
+
+	assign afunc3[2'b00] = gpr[src8] + {7'b0, f_c};			// adc
+	assign afunc3[2'b01] = gpr[src8] - {7'b0, f_c};			// sbc
+	assign afunc3[2'b10] = {gpr[src8][3:0],
+							gpr[src8][7:4]};				// swap
+	assign afunc3[2'b11] = {gpr[src8][0], gpr[src8][1],
+							gpr[src8][2], gpr[src8][3],
+							gpr[src8][4], gpr[src8][5],
+							gpr[src8][6], gpr[src8][7]};	// flip
+
+	
+//- Jump Condition decoding -----------------------------------
+	wire jcond[0:15];
+
+	assign jcond[4'b0000] = 1'b1;				//always
+	assign jcond[4'b0001] = _z;             	//equal, Z
+	assign jcond[4'b0010] = _c;             	//less, C
+	assign jcond[4'b0011] = !_z;            	//not equal, !Z
+	assign jcond[4'b0100] = !_c && !_z;     	//greater, !C&!Z
+	assign jcond[4'b0101] = _c || _z;       	//less of equal, C|Z
+	assign jcond[4'b0110] = !_c;            	//greater or equal, !C
+	assign jcond[4'b0111] = _n;             	//negative, N
+	assign jcond[4'b1000] = !_n;            	//positive, !N
+	assign jcond[4'b1001] = 1'b1;               //signed less
+	assign jcond[4'b1010] = 1'b1;               //signed less or equal
+	assign jcond[4'b1011] = 1'b1;               //signed greater
+	assign jcond[4'b1100] = 1'b1;               //signed greater or equal
+	assign jcond[4'b1101] = _h;             	//half carry, H
+	assign jcond[4'b1110] = !_h;            	//not half carry, !H
+	assign jcond[4'b1111] = 1'b0;           	//never
+
+
+//- Wait Condition decoding -----------------------------------
+	wire wcond[0:3];
+
+	assign wcond[2'b00] = cbeg;
+	assign wcond[2'b01] = post_cbeg;
+	assign wcond[2'b10] = pre_cend;       
+	assign wcond[2'b11] = cend;      
+
+
+//- Pins in decoding ------------------------------------------
+	wire pin_r[0:31];
+	
+	assign pin_r[5'h00] = 1'b1;		
+	assign pin_r[5'h01] = timer_end;
+	assign pin_r[5'h02] = apu_strobe;
+	assign pin_r[5'h03] = hsync_start;
+	assign pin_r[5'h04] = hblank;
+	assign pin_r[5'h05] = vsync;
+	assign pin_r[5'h06] = vblank;
+	assign pin_r[5'h07] = line_start;
+	assign pin_r[5'h08] = 1'bX;
+	assign pin_r[5'h09] = 1'bX;
+	assign pin_r[5'h0A] = 1'bX;
+	assign pin_r[5'h0B] = 1'bX;
+	assign pin_r[5'h0C] = 1'bX;
+	assign pin_r[5'h0D] = 1'bX;
+	assign pin_r[5'h0E] = 1'bX;
+	assign pin_r[5'h0F] = 1'bX;
+	assign pin_r[5'h10] = 1'bX;		
+	assign pin_r[5'h11] = 1'bX;
+	assign pin_r[5'h12] = 1'bX;
+	assign pin_r[5'h13] = 1'bX;
+	assign pin_r[5'h14] = 1'bX;
+	assign pin_r[5'h15] = 1'bX;
+	assign pin_r[5'h16] = 1'bX;
+	assign pin_r[5'h17] = 1'bX;
+	assign pin_r[5'h18] = 1'bX;
+	assign pin_r[5'h19] = 1'bX;
+	assign pin_r[5'h1A] = 1'bX;
+	assign pin_r[5'h1B] = 1'bX;
+	assign pin_r[5'h1C] = 1'bX;
+	assign pin_r[5'h1D] = 1'bX;
+	assign pin_r[5'h1E] = 1'bX;
+	assign pin_r[5'h1F] = 1'bX;
+
+
+//- Ports in decoding ------------------------------------------
+	wire [31:0] port_r[0:15];
+	
+	assign port_r[4'h00] = { 24'b0, sram_data_r		};	// sram_data_r
+	assign port_r[4'h01] = { 16'b0, apu_data_r		};	// apu_data_r
+	assign port_r[4'h02] = { 16'b0, timer_data_r	};	// timer_data_r
+	assign port_r[4'h03] = { 16'b0, hcnt			};	// hcnt
+	assign port_r[4'h04] = { 16'b0, vcnt			};	// vcnt
+	assign port_r[4'h05] = { 32'b0 					};	// 
+	assign port_r[4'h06] = { 32'b0 					};	// 
+	assign port_r[4'h07] = { 32'b0 					};	// 
+	assign port_r[4'h08] = { 32'b0 					};	// 
+	assign port_r[4'h09] = { 32'b0 					};	// 
+	assign port_r[4'h0A] = { 32'b0 					};	// 
+	assign port_r[4'h0B] = { 32'b0 					};	// 
+	assign port_r[4'h0C] = { 32'b0 					};	// 
+	assign port_r[4'h0D] = { 32'b0 					};	// 
+	assign port_r[4'h0E] = { 32'b0 					};	// 
+	assign port_r[4'h0F] = { 32'b0 					};	// 
+	
+	
+	wire [15:0] port_r_e[0:15];
+	
+//						reg	=	  3210	   3210		3210	 3210
+//						sz	=	  3333	   2222		1111	 0000
+	assign port_r_e[4'h00] = { 4'b0001, 4'b0001, 4'b0001, 4'b0001 };	// sram_data_r
+	assign port_r_e[4'h01] = { 4'b0011, 4'b0011, 4'b0010, 4'b0001 };	// apu_data_r
+	assign port_r_e[4'h02] = { 4'b0011, 4'b0011, 4'b0010, 4'b0001 };	// timer_data_r
+	assign port_r_e[4'h03] = { 4'b0011, 4'b0011, 4'b0010, 4'b0001 };	// hcnt
+	assign port_r_e[4'h04] = { 4'b0011, 4'b0011, 4'b0010, 4'b0001 };	// vcnt
+	assign port_r_e[4'h05] = { 4'b0000, 4'b0000, 4'b0000, 4'b0000 };	// 
+	assign port_r_e[4'h06] = { 4'b0000, 4'b0000, 4'b0000, 4'b0000 };	// 
+	assign port_r_e[4'h07] = { 4'b0000, 4'b0000, 4'b0000, 4'b0000 };	// 
+	assign port_r_e[4'h08] = { 4'b0000, 4'b0000, 4'b0000, 4'b0000 };	// 
+	assign port_r_e[4'h09] = { 4'b0000, 4'b0000, 4'b0000, 4'b0000 };	// 
+	assign port_r_e[4'h0A] = { 4'b0000, 4'b0000, 4'b0000, 4'b0000 };	// 
+	assign port_r_e[4'h0B] = { 4'b0000, 4'b0000, 4'b0000, 4'b0000 };	// 
+	assign port_r_e[4'h0C] = { 4'b0000, 4'b0000, 4'b0000, 4'b0000 };	// 
+	assign port_r_e[4'h0D] = { 4'b0000, 4'b0000, 4'b0000, 4'b0000 };	// 
+	assign port_r_e[4'h0E] = { 4'b0000, 4'b0000, 4'b0000, 4'b0000 };	// 
+	assign port_r_e[4'h0F] = { 4'b0000, 4'b0000, 4'b0000, 4'b0000 };	// 
 
 	
 //- PC handling -----------------------------------------------
@@ -145,6 +318,8 @@ wt_pin:		// wait	cond pin0 mask pin1
 		pc_next = ~cond ^^ (~mask ? pin[pin0] || pin[pin1] : pin[pin0] && pin[pin1]) ? pc : pc_inc;
 
 wt_port:	// wait sz (port) wc src
+		begin
+		end
 
 jr:			// jr jc, rel8
 		pc_next = jcond[jc] ? pc_rel : pc_inc;
@@ -159,46 +334,6 @@ default:
 		pc_next = pc_inc;
 	
 	endcase
-
-
-//- Jump Condition decoding -----------------------------------
-	reg jcond;
-
-	localparam	c_always		= 4'b0000;	//always
-	localparam	c_equal			= 4'b0001;	//equal, Z
-	localparam	c_less			= 4'b0010;	//less, C
-	localparam	c_nequal		= 4'b0011;	//not equal, !Z
-	localparam	c_greater		= 4'b0100;	//greater, !C&!Z
-	localparam	c_lessequal		= 4'b0101;	//less of equal, C|Z
-	localparam	c_greaterequal	= 4'b0110;	//greater or equal, !C
-	localparam	c_negative		= 4'b0111;	//negative, N
-	localparam	c_positive		= 4'b1000;	//positive, !N
-	localparam	c_sless			= 4'b1001;	//signed less
-	localparam	c_slessequal	= 4'b1010;	//signed less or equal
-	localparam	c_sgreater		= 4'b1011;	//signed greater
-	localparam	c_sgreaterequal	= 4'b1100;	//signed greater or equal
-	localparam	c_hcarry		= 4'b1101;	//half carry, H
-	localparam	c_nhcarry		= 4'b1110;	//not half carry, !H
-	localparam	c_never			= 4'b1111;	//never
-                
-	wire jcond[0:15]
-
-	assign jcond[c_always		 ] = 1'b1;
-	assign jcond[c_equal		 ] = _z;
-	assign jcond[c_less		 	 ] = _c;
-	assign jcond[c_nequal		 ] = !_z;
-	assign jcond[c_greater		 ] = !_c && !_z;
-	assign jcond[c_lessequal	 ] = _c || _z;
-	assign jcond[c_greaterequal  ] = !_c;
-	assign jcond[c_negative	 	 ] = _n;
-	assign jcond[c_positive	 	 ] = !_n;
-	assign jcond[c_sless		 ] = 
-	assign jcond[c_slessequal	 ] = 
-	assign jcond[c_sgreater	 	 ] = 
-	assign jcond[c_sgreaterequal ] = 
-	assign jcond[c_hcarry		 ] = _h;
-	assign jcond[c_nhcarry		 ] = !_h;
-	assign jcond[c_never		 ] = 1'b0;
 
 
 //- Ports control decoding ------------------------------------
@@ -220,15 +355,15 @@ default:
 	wire [1:0] psz_w[0:15];
 
 	// wires
-	wire [7:0]	sram_data_w		= r[dst];
-	wire [15:0]	timer_data_w	= {r[dst16+1], r[dst16]};
-	wire [7:0]	apu_covox		= r[dst];
-	wire [5:0]	apu_border		= r[dst][5:0];
+//	assign [7:0]	sram_data_w		= gpr[dst8];
+//	assign [15:0]	timer_data_w	= {gpr[dst16+1], r[dst16]};
+//	assign [7:0]	apu_covox		= gpr[dst8];
+//	assign [5:0]	apu_border		= gpr[dst8][5:0];
 
 	// regs
-	wire [15:0] sram_addr		= port_w[p_sram_addr][15:0];
-	wire [20:0] apu_addr		= port_w[p_apu_addr][21:1];
-	wire [15:0] apu_data_w		= port_w[p_apu_data_w][15:0];
+//	assign [15:0] sram_addr		= port_w[p_sram_addr][15:0];
+//	assign [20:0] apu_addr		= port_w[p_apu_addr][21:1];
+//	assign [15:0] apu_data_w		= port_w[p_apu_data_w][15:0];
 
 	// sizes
 	assign psz_w	[p_sram_addr]		= s16;
@@ -240,10 +375,10 @@ default:
 	assign psz_w	[p_apu_border]		= s8;
 	
 	// WEs - all should be sampled by fclk
-	wire sram_we		= port == p_sram_data_w;
-	wire timer_we		= port == p_timer_data_w;
-	wire apu_covox_we	= port == p_apu_covox;
-	wire apu_border_we	= port == p_apu_border;
+	assign sram_we		= port == p_sram_data_w;
+	assign timer_we		= port == p_timer_data_w;
+	assign apu_covox_we	= port == p_apu_covox;
+	assign apu_border_we	= port == p_apu_border;
 
 	always @(posedge clk)
 	begin
@@ -260,29 +395,6 @@ default:
 	end
 
 
-//- Ports-in decoding -----------------------------------------
-	localparam p_sram_data_r	= 4'h0;
-	localparam p_apu_data_r		= 4'h1;
-	localparam p_timer_data_r	= 4'h2;
-	localparam p_hcnt			= 4'h3;
-	localparam p_vcnt			= 4'h4;
-	
-	wire [31:0] port_r[0:15];
-	wire [1:0] psz_r[0:15];
-
-	assign port_r	[p_sram_data_r 	][7:0]	= sram_data_r;
-	assign port_r	[p_apu_data_r  	][15:0]	= apu_data_r;
-	assign port_r	[p_timer_data_r	][15:0]	= timer_data_r;
-	assign port_r	[p_hcnt		   	][15:0]	= hcnt;
-	assign port_r	[p_vcnt		   	][15:0]	= vcnt;
-
-	assign psz_r	[p_sram_data_r 	]		= s8;
-	assign psz_r	[p_apu_data_r  	]		= s16;
-	assign psz_r	[p_timer_data_r	]		= s16;
-	assign psz_r	[p_hcnt		   	]		= s16;
-	assign psz_r	[p_vcnt		   	]		= s16;
-
-
 //- Pins out decoding and clocking ----------------------------
 	localparam p_apu_req		= 5'h0;	//data: outreg
 	localparam p_apu_dram_bsel0	= 5'h1;	//data: outreg
@@ -291,9 +403,9 @@ default:
 
 	reg pin_w[0:31];		//output pins
 	
-	wire 		apu_req			= pin_w[p_apu_req];
-	wire [1:0]	apu_dram_bsel	= {pin_w[apu_dram_bsel1], pin_w[apu_dram_bsel0]};
-	wire		apu_dram_rnw	= pin_w[p_apu_dram_rnw];
+//	assign 		apu_req			= pin_w[p_apu_req];
+//	assign [1:0]	apu_dram_bsel	= {pin_w[apu_dram_bsel1], pin_w[apu_dram_bsel0]};
+//	assign		apu_dram_rnw	= pin_w[p_apu_dram_rnw];
 	
 	always @(posedge clk)
 	begin
@@ -305,40 +417,13 @@ default:
 	end
 
 
-//- Pins in decoding ------------------------------------------
-	localparam p_timer_end	= 5'h0;
-	localparam p_apu_strobe	= 5'h1;
-	localparam p_hsync_start= 5'h2;
-	localparam p_hblank		= 5'h3;
-	localparam p_vsync		= 5'h4;
-	localparam p_vblank		= 5'h5;
-	localparam p_cbeg		= 5'h6;
-	localparam p_post_cbeg	= 5'h7;
-	localparam p_pre_cend	= 5'h8;
-	localparam p_cend		= 5'h9;
-
-	wire p_in[0:31];		//input pins
-	
-	wire 		apu_req			= p[p_apu_req];
-	wire [1:0]	apu_dram_bsel	= {p[apu_dram_bsel1], p[apu_dram_bsel0]};
-	wire		apu_dram_rnw	= p[p_apu_dram_rnw];
-	
-	always @(posedge clk)
-	begin
-		if (ld_pin0)
-			p[pin0] <= bit0;
-
-		if (ld_pin1)
-			p[pin1] <= bit1;
-	end
-
-	
 //- PC clocking -----------------------------------------------
 	always @(posedge clk)
 		{r[15], r[14]} <= pc_next;
 
 
 //- Registers, flags clocking ---------------------------------
+	reg _c, _z, _n;			// flags
 	reg [7:0] r[0:15];		// GPRs
 	reg z, c, n;			// flags
 	
@@ -350,11 +435,10 @@ default:
 		
 		if (ld_dst)
 			r[dst] <= res;	// RESTRICTION! Should be SZ added!
+	end
 	
 
 //- APU Halting -----------------------------------------------
-	wire [15:0] instr;
-
 	always @*
 	if (apu_halt)
 		instr = 16'hfffe;
@@ -395,13 +479,15 @@ default:
 
 //- Timer module ----------------------------------------------
 	reg timer_wen;
-					
+	wire [15:0]	timer_data_r;
+	wire timer_end;
+	
 	apu_timer apu_timer(
 					.clk(clk),
 					.wdata(timer_data_w),
-					.wen(timer_we)
+					.wen(timer_we),
 					.ctr(timer_data_r),
 					.cnt_end(timer_end)
-			)
+			);
 
 endmodule
