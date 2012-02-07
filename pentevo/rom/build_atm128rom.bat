c340d8285cb6c8e7396e46e4935ce3d8cee2fa80
at2313 <at2313@gmail.com>
2012-02-07 07:09:41 +0000

    BIOS: removed nedoPC ROMs, stub

--- pentevo/rom/build_atm128rom.bat
+++
@@ -1,7 +0,0 @@
-
-copy /B /Y page3\*.rom+page1\evo-dos_virt.rom+page2\128_low.rom+page0\services.rom+page3\*.rom+page1\evo-dos_emu3d13.rom+page2\128_low.rom+page0\services.rom zxevo_atm128rom.rom
-REM copy /B /Y page3\*.rom+page1\evo-dos_virt.rom+page2\fatall4rom.rom+page0\services.rom+page3\*.rom+page1\evo-dos_emu3d13.rom+page2\fatall4rom.rom+page0\services.rom zxevo_atm128rom.rom
-
-rem copy /B /Y zxevo_atm128rom.rom d:\unrealspeccy\
-
-pause
