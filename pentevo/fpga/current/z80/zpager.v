0040984b4dc918fa5aac191d86d10efd09f98520
at2313 <at2313@gmail.com>
2012-02-19 07:40:04 +0000

    TSconf: fixed dos turn off when vdos=on

--- pentevo/fpga/current/z80/zpager.v
+++ pentevo/fpga/current/z80/zpager.v
@@ -31,14 +31,14 @@ module pager (
 
     wire [1:0] win = za[15:14];
     wire win0 = ~|win;
-    // assign romnram = win0 & ~memconf[3] & !vdos;
-    assign romnram = win0 & ~memconf[3] & !(vdos & dos);
+    assign romnram = win0 & ~memconf[3] & !vdos;
+    // assign romnram = win0 & ~memconf[3] & !(vdos & dos);
     assign rw_en = !win0 | memconf[1];
     assign page = xtpage[win];
     
     wire [7:0] xtpage[0:3];
-    // assign xtpage[0] = vdos ? 8'hFF : {xt_page[7:2], memconf[2] ? xt_page[1:0] : {~dos, memconf[0]}};
-    assign xtpage[0] = (vdos & dos) ? 8'hFF : {xt_page[7:2], memconf[2] ? xt_page[1:0] : {~dos, memconf[0]}};
+    assign xtpage[0] = vdos ? 8'hFF : {xt_page[7:2], memconf[2] ? xt_page[1:0] : {~dos, memconf[0]}};
+    // assign xtpage[0] = (vdos & dos) ? 8'hFF : {xt_page[7:2], memconf[2] ? xt_page[1:0] : {~dos, memconf[0]}};
     assign xtpage[1] = xt_page[15:8];
     assign xtpage[2] = xt_page[23:16];
     assign xtpage[3] = xt_page[31:24];
@@ -46,7 +46,7 @@ module pager (
     
 // DOS signal control
 	assign dos_on = win0 && m1 && mreq && (za[13:8]==6'h3D) && memconf[0];
-	assign dos_off = !win0 && m1 && mreq;
+	assign dos_off = !win0 && m1 && mreq && !vdos;
     // assign zclk_stall = dos_on | stall_count[2];
     assign zclk_stall = 0;
 
