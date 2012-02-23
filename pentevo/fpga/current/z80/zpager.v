f64c91c650226b37f0f715df5010955778af8ece
at2313 <at2313@gmail.com>
2012-02-23 21:07:57 +0000

    TSconf: fixed WE for vdos (follow-up)

--- pentevo/fpga/current/z80/zpager.v
+++ pentevo/fpga/current/z80/zpager.v
@@ -32,13 +32,11 @@ module pager (
     wire [1:0] win = za[15:14];
     wire win0 = ~|win;
     assign romnram = win0 & ~memconf[3] & !vdos;
-    // assign romnram = win0 & ~memconf[3] & !(vdos & dos);
-    assign rw_en = !win0 | memconf[1] | (win0 & vdos);
+    assign rw_en = !win0 | memconf[1] | vdos;
     assign page = xtpage[win];
 
     wire [7:0] xtpage[0:3];
     assign xtpage[0] = vdos ? 8'hFF : {xt_page[7:2], memconf[2] ? xt_page[1:0] : {~dos, memconf[0]}};
-    // assign xtpage[0] = (vdos & dos) ? 8'hFF : {xt_page[7:2], memconf[2] ? xt_page[1:0] : {~dos, memconf[0]}};
     assign xtpage[1] = xt_page[15:8];
     assign xtpage[2] = xt_page[23:16];
     assign xtpage[3] = xt_page[31:24];
