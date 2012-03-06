6c1adebe42c3a97e9699aa71f5de226febd0d654
at2313 <at2313@gmail.com>
2012-03-06 21:10:44 +0000

    TSconf: Auto-lock128 changed to M1 D7 checking

--- pentevo/fpga/current/z80/zports.v
+++ pentevo/fpga/current/z80/zports.v
@@ -13,6 +13,7 @@ module zports(
 	input  wire [15:0] a,
 
 	input  wire        rst,     // system reset
+	input  wire        m1,
 
 	input  wire        rd,
 	input  wire        wr,
@@ -373,7 +374,13 @@ module zports(
 	reg [7:0] rampage[0:3];
 	assign xt_page = {rampage[3], rampage[2], rampage[1], rampage[0]};
 
-    wire lock128 = memconf[7] ? !a[13] : memconf[6];
+    wire lock128 = memconf[7] ? m1_bit7 : memconf[6];
+    
+	reg m1_bit7;
+    always @(posedge clk)
+		if (m1)
+			m1_bit7 <= din[7];
+
         
 	always @(posedge clk)
 		if (rst)
