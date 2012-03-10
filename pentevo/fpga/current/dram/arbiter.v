1a02c4f3d4e0e23bb07a7b4230ec0ef87c7789fa
at2313 <at2313@gmail.com>
2012-03-10 02:08:50 +0000

    TSconf: arbiter works with programmable priorities between CPU and DMA/TS BIOS: fixed DMA burst waiting by dummy RAM access (Note: first program new BIOS, then firmare!!!)

--- pentevo/fpga/current/dram/arbiter.v
+++ pentevo/fpga/current/dram/arbiter.v
@@ -2,15 +2,15 @@
 
 // PentEvo project (c) NedoPC 2008-2011
 //
-// DRAM arbiter. Shares DRAM between processor and video data fetcher
+// DRAM arbiter. Shares DRAM between CPU, video data fetcher and other devices
 //
 
 // Arbitration is made on full 8-cycle access blocks. Each cycle is defined by dram.v and consists of 4 fpga clocks.
-// During each access block, there can be either no videodata access, 1 videodata access, 2, 4 or full 8 accesses.
-// All spare cycles can be used by processor. If nobody uses memory in the given cycle, refresh cycle is performed
+// During each access block, there can be either no videodata access, 1 videodata access, 2, 4 or 8 accesses.
+// All spare cycles can be used by CPU or other devices. If no device uses memory in the given cycle, refresh cycle is performed.
 //
-// In each access block, videodata accesses are spreaded all over the block so that processor receives cycle
-// as fast as possible, until there is absolute need to fetch remaining video data
+// In each access block, videodata accesses are spreaded all over the block so that CPU receives cycle
+// as fast as possible, until there is absolute need to fetch remaining video data.
 //
 // Examples:
 //
@@ -35,8 +35,18 @@
 //
 // access block begins at any dram cycle, then blocks go back-to-back
 //
-// key signals are go and cpu_req, sampled at the end of each dram cycle. Must be set to the module
-// one clock cycle earlier the clock of the beginning current dram cycle
+// key signals are go and XXX_req, sampled at the end of each dram cycle. Must be set to the module at c3 clock cycle.
+
+// CPU can have either normal or lower access priority to the DRAM.
+// At the INT active (32 of 3.5MHz clocks) the priority is raised to normal, so that CPU won't miss its interrupt.
+// This should be considered if dummy RAM access used for waiting for the end of DMA operation instead of status bit polling.
+//
+// DRAM access priority:
+// Z80 normal       Z80 low
+// - VIDEO          - VIDEO
+// - CPU            - TS
+// - TS             - DMA
+// - DMA            - CPU
 
 
 module arbiter(
@@ -55,15 +65,6 @@ module arbiter(
 	output wire  [1:0] dram_bsel,   // positive bytes select: bsel[1] for wrdata[15:8], bsel[0] for wrdata[7:0]
 	output wire [15:0] dram_wrdata, // data to be written
 
-// CPU
-	input wire [20:0] cpu_addr,
-	input wire [ 7:0] cpu_wrdata,
-	input wire        cpu_req,
-	input wire        cpu_rnw,
-	input wire        cpu_wrbsel,
-	output reg        cpu_next,
-    output reg        cpu_strobe,
-
 // video
 	input  [20:0] video_addr,   // during access block, only when video_strobe==1
 	output wire   video_next,   // on this signal you can change video_addr; it is one clock leading the video_strobe
@@ -73,6 +74,14 @@ module arbiter(
 	input wire [4:0] video_bw,
 								// [4:3] - total cycles: 11 = 8 / 01 = 4 / 00 = 2
 								// [2:0] - need cycles
+// CPU
+	input wire [20:0] cpu_addr,
+	input wire [ 7:0] cpu_wrdata,
+	input wire        cpu_req,
+	input wire        cpu_rnw,
+	input wire        cpu_wrbsel,
+	output reg        cpu_next,
+    output wire       cpu_strobe,
 
 // DMA
 	input wire [20:0] dma_addr,
@@ -82,7 +91,7 @@ module arbiter(
 	input wire        dma_rnw,
 	output reg        dma_next,
 
-// TS engine
+// TS
 	input wire [20:0] ts_addr,
 	input wire 	      ts_req,
 	input wire 	      ts_zwt,
@@ -90,14 +99,6 @@ module arbiter(
 	output wire       ts_next
 
 );
-
-
-// DRAM access priority:
-// Z80 normal       Z80 low
-// - VIDEO          - VIDEO
-// - CPU            - TS
-// - TS             - DMA
-// - DMA            - CPU
 
 
 	localparam CYC_CPU   = 4'b0001;
@@ -140,20 +141,22 @@ module arbiter(
 
 // track vid_rem counter
 // how many video cycles left to the end of block (7..0)
-	wire vid_nrem = (go && blk_start) ? (cpu_req ? vidmax : (vidmax - 3'd1)) : (next_vid ? vid_nrem_next : vid_rem);
+	wire [2:0] vid_nrem = (go && blk_start) ? (other_req ? vidmax : (vidmax - 3'd1)) : (next_vid ? vid_nrem_next : vid_rem);
 	wire [2:0] vidmax = {video_bw[2:0]};    // number of cycles for video access
 	wire [2:0] vid_nrem_next = ~|vid_rem ? 3'd0 : (vid_rem - 3'd1);
 
-	reg [2:0] vid_rem;  // remaining video accesses in block (4..0)
-	always @(posedge clk) if (c3)       // number of remain video cycles
+	reg [2:0] vid_rem;      // remaining video accesses in block
+	always @(posedge clk) if (c3)
 		vid_rem <= vid_nrem;
 
 
 // next cycle decision
     wire cpu_low = ((ts_req & ts_zwt) | (dma_req & dma_zwt)) & int_n;
     wire other_req = cpu_req | ts_req | dma_req;
-    wire [3:0] next_other = cpu_low ? (ts_req ? CYC_TS : CYC_DMA) : CYC_CPU;
-    wire video_only = stall | (vid_rem==blk_rem);
+    wire [3:0] next_other = cpu_low ? next_other_low : next_other_norm;
+    wire [3:0] next_other_norm = cpu_req ? CYC_CPU : (ts_req ? CYC_TS : CYC_DMA);
+    wire [3:0] next_other_low = ts_req ? CYC_TS : (dma_req ? CYC_DMA : CYC_CPU);
+    wire video_only = stall | (vid_rem == blk_rem);
 
 	always @*
 		if (blk_start)      // video burst start
@@ -172,7 +175,7 @@ module arbiter(
 		else                // video burst in progress
 		begin
 			cpu_next = video_only ? 1'b0 : !cpu_low;
-			next_cycle = video_only ? CYC_VIDEO : (other_req ? next_other : (|vid_rem ? CYC_VIDEO : CYC_FREE);
+			next_cycle = video_only ? CYC_VIDEO : (other_req ? next_other : (|vid_rem ? CYC_VIDEO : CYC_FREE));
 		end
 
 
@@ -189,19 +192,13 @@ module arbiter(
 	assign dram_req = !next_fre;
     assign dram_rnw = next_cpu ? cpu_rnw : next_dma ? dma_rnw : 1'b1;
 
-
-// generation of read strobes: for video and cpu
 	reg cpu_rnw_r;
 	always @(posedge clk) if (c3)
 		cpu_rnw_r <= cpu_rnw;
 
 
-	always @(posedge clk)
-		if (curr_cpu && cpu_rnw_r && c2)
-			cpu_strobe <= 1'b1;
-		else
-			cpu_strobe <= 1'b0;
-
+// generation of read strobes: for video and cpu
+    assign cpu_strobe = curr_cpu && cpu_rnw_r && c3;
 
 	assign video_next = curr_vid & c2;
 	assign video_strobe = curr_vid & c3;
@@ -213,4 +210,3 @@ module arbiter(
 
 
 endmodule
-
