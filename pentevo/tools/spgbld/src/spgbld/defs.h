92e40a78592d97da5640c633a5e09bad37e76a8b
at2313 <at2313@gmail.com>
2012-09-06 19:36:06 +0000

    Tools: added SPG creator Docs: added SPG 1.0 format

---
+++ pentevo/tools/spgbld/src/spgbld/defs.h
@@ -0,0 +1,91 @@
+#pragma once
+
+typedef unsigned long long u64;
+typedef signed long long i64;
+typedef unsigned long u32;
+typedef signed long i32;
+typedef unsigned short u16;
+typedef signed short i16;
+typedef unsigned char u8;
+typedef signed char i8;
+
+#define min(a,b)            (((a) < (b)) ? (a) : (b))
+
+#define STR_EXPAND(tok) #tok
+#define STR(tok) STR_EXPAND(tok)
+
+// .spg defaults
+#define SPG_MAGIC	SpectrumProg
+#define SPG_VER		1
+#define SPG_SUBVER	0
+#define SPG_PACK	1
+#define SPG_RES		0x5B00
+#define SPG_SP		0x5C00
+#define SPG_PAGE3	0
+#define SPG_CLK		0
+#define SPG_INT		0
+#define SPG_PGR		0
+
+// .ini fields
+#define F_DESC 	desc
+#define F_START start
+#define F_SP	stack
+#define F_PAGE3 page3
+#define F_CLOCK clock
+#define F_INT 	int
+#define F_PAGER pager
+#define F_RES	resident
+#define F_DAY 	day
+#define F_MONTH month
+#define F_YEAR 	year
+#define F_BLK  	block
+
+struct CONF {
+	int packer;
+	int n_blocks;
+};
+
+struct HDR {
+	char desc[32];
+	char magic[12];
+	struct {
+		u8 subver:4;
+		u8 ver:4;
+	};
+	u8 day;
+	u8 month;
+	u8 year;
+	u16 start;
+	u16 sp;
+	u8 page3;
+	struct {
+		u8 clk:2;
+		u8 ei:1;
+	};
+	u16 pager;
+	u16 resid;
+	u16 num_blk;
+	u8 second;
+	u8 minute;
+	u8 hour;
+	u8 pad_0[17];
+	char creator[32];
+	u8 pad_1[144];
+	struct {
+		struct {
+			u8 addr:5;
+			u8 pad_1:2;
+			u8 last:1;
+			u8 size:5;
+			u8 pad_2:1;
+			u8 comp:2;
+		};
+		u8 page;
+	} blk[256];
+};
+
+struct BLK {
+	char fname[128];
+	int size;
+	u8 data[16384];
+};
---
+++ pentevo/tools/spgbld/src/spgbld/func.h
@@ -0,0 +1,8 @@
+#pragma once
+
+void init_hdr();
+void load_ini(_TCHAR*);
+void load_files();
+void pack_blocks();
+void save_out(_TCHAR*);
+void rand_name(char*);
