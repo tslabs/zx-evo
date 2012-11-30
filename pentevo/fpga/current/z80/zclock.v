0f2b04c82df7c999fa09c149bd7ee371c1096adf
at2313 <at2313@gmail.com>
2012-11-30 23:30:10 +0000

    TSconf: Z80 fix pre-commit

--- pentevo/fpga/current/z80/zclock.v
+++ pentevo/fpga/current/z80/zclock.v
@@ -3,7 +3,7 @@
 // Z80 clocking module, also contains some wait-stating when 14MHz
 //
 // IDEAL:
-// fclk    _/`\_/`\_/`\_/`\_/`\_/`\_/`\_/`\_/`\_/`\_/`\_/`\_/`\_/`\_/`\_/`\_/`\_/`\_/`\_/`\_/`\_/`\_/`\_/`\_/`\_/`\_/`\_/`\_/`\_/`\_/`\
+// clk    _/`\_/`\_/`\_/`\_/`\_/`\_/`\_/`\_/`\_/`\_/`\_/`\_/`\_/`\_/`\_/`\_/`\_/`\_/`\_/`\_/`\_/`\_/`\_/`\_/`\_/`\_/`\_/`\_/`\_/`\_/`\
 //          |   |   |   |   |   |   |   |   |   |   |   |   |   |   |   |   |   |   |   |   |   |   |   |   |   |   |   |   |   |   |
 // zclk     /```\___/```\___/```\___/```````\_______/```````\_______/```````````````\_______________/```````````````\_______________/`
 //          |   |   |   |   |   |   |   |   |   |   |   |   |   |   |   |   |   |   |   |   |   |   |   |   |   |   |   |   |   |   |
@@ -26,13 +26,11 @@
 
 module zclock(
 
-	input fclk,
+	input clk,
 	input zclk, // Z80 clock, buffered via act04 and returned back to the FPGA
 	output reg zclk_out, // generated Z80 clock - passed through inverter externally!
 	input c0, c2,
-    
-	input rst,
-	input rfsh_n, // switch turbo modes in RFSH part of m1
+
 	input  wire iorq_s,
 	input  wire external_port,
 
@@ -43,35 +41,19 @@ module zclock(
 	input  wire cpu_stall,
 	input  wire ide_stall,
 	input  wire dos_on,
-	input  wire dos_off,
+	input  wire vdos_off,
 
-	input [1:0] turbo, // 2'b00 -  3.5 MHz
+	input [1:0] turbo  // 2'b00 -  3.5 MHz
 	                   // 2'b01 -  7.0 MHz
 	                   // 2'b1x - 14.0 MHz
-
-	// output reg [1:0] int_turbo // internal turbo, switched on /RFSH
-	output wire [1:0] int_turbo // internal turbo, switched on /RFSH
-    
 );
-
-	reg c2_cnt;
-	wire h_c2_1; // to take every other pulse of c2
-	wire h_c2_2; // to take every other pulse of c2
-
-	reg [2:0] zcount; // counter for generating 3.5 and 7 MHz z80 clocks
-
-
-	// reg old_rfsh_n;
-
-
-	reg clk14_src; // source for 14MHz clock
 
 
 `ifdef SIMULATE
 	initial // simulation...
 	begin
 		c2_cnt = 1'b0;
-		int_turbo   = 2'b00;
+		turbo   = 2'b00;
 		old_rfsh_n  = 1'b1;
 		clk14_src   = 1'b0;
 
@@ -79,23 +61,13 @@ module zclock(
 	end
 `endif
 
-	// switch clock only at predefined time
-	// always @(posedge fclk) if(zpos)
-	// begin
-		// old_rfsh_n <= rfsh_n;
-
-		// if( old_rfsh_n && !rfsh_n )
-			// int_turbo <= turbo;
-	// end
-		assign int_turbo = turbo;
-
 
 // wait generator
-	wire stall = cpu_stall || dos_io_stall || ide_stall;
     wire dos_io_stall = stall_start || !stall_count_end;
     wire stall_start = dos_stall || io_stall;
     wire dos_stall = dos_on || vdos_off;
-    wire io_stall = iorq_s && external_port && int_turbo[1];
+    // wire io_stall = iorq_s && external_port && turbo[1];
+    wire io_stall = iorq_s;
 	wire stall_count_end = stall_count[3];
 
 	reg [3:0] stall_count;
@@ -104,57 +76,55 @@ module zclock(
 		begin
 			if (dos_stall)
 				stall_count <= 4'd4;	// 4 tacts 28MHz (1 tact 7MHz)
-			else
+			else if (io_stall)
 				stall_count <= 4'd0;	// 8 tacts 28MHz (1 tact 3.5MHz)
 		end
 		else if (!stall_count_end)
 			stall_count <= stall_count + 3'd1;
 
 
-	// 14MHz clocking
-	always @(posedge fclk)
-	if( !stall )
+// Z80 clocking pre-strobes
+	wire pre_zpos = turbo[1] ? pre_zpos_140 : ( turbo[0] ? pre_zpos_70 : pre_zpos_35 );
+	wire pre_zneg = turbo[1] ? pre_zneg_140 : ( turbo[0] ? pre_zneg_70 : pre_zneg_35 );
+
+	reg clk14_src;	// source for 14MHz clock
+	always @(posedge clk)
+	if (!stall)
 		clk14_src <= ~clk14_src;
-	//
-	wire pre_zpos_140 =   clk14_src ;
-	wire pre_zneg_140 = (~clk14_src);
 
-
-	// take every other pulse of c2 (make half c2)
-	always @(posedge fclk) if( c2 )
-		c2_cnt <= ~c2_cnt;
-
-	assign h_c2_1 =  c2_cnt && c2;
-	assign h_c2_2 = !c2_cnt && c2;
-
-	wire pre_zpos_35 = h_c2_2;
-	wire pre_zneg_35 = h_c2_1;
+	wire pre_zpos_140 = clk14_src;
+	wire pre_zneg_140 = ~clk14_src;
 
 	wire pre_zpos_70 = c2;
 	wire pre_zneg_70 = c0;
 
-	wire pre_zpos = int_turbo[1] ? pre_zpos_140 : ( int_turbo[0] ? pre_zpos_70 : pre_zpos_35 );
-	wire pre_zneg = int_turbo[1] ? pre_zneg_140 : ( int_turbo[0] ? pre_zneg_70 : pre_zneg_35 );
+	wire pre_zpos_35 = c2_cnt && c2;
+	wire pre_zneg_35 = !c2_cnt && c2;
 
+	reg c2_cnt;
+	always @(posedge clk) if (c2)
+		c2_cnt <= ~c2_cnt;
 
-	always @(posedge fclk)
+		
+// Z80 clocking strobes
+	// wire stall = cpu_stall || ide_stall;
+	wire stall = cpu_stall || dos_io_stall || ide_stall;
+
+	always @(posedge clk)
 	begin
-		zpos <= (~stall) & pre_zpos & zclk_out;
+		zpos <= !stall && pre_zpos && zclk_out;
+		zneg <= !stall && pre_zneg && !zclk_out;
 	end
 
-	always @(posedge fclk)
-	begin
-		zneg <= (~stall) & pre_zneg & (~zclk_out);
-	end
-
-
+	
 	// make Z80 clock: account for external inversion and make some leading of clock
-	// 9.5 ns propagation delay: from fclk posedge to zclk returned back any edge
+	// 9.5 ns propagation delay: from clk posedge to zclk returned back any edge
 	// (1/28)/2=17.9ns half a clock lead
 	// 2.6ns lag because of non-output register emitting of zclk_out
-	// total: 5.8 ns lead of any edge of zclk relative to posedge of fclk => ACCOUNT FOR THIS WHEN DOING INTER-CLOCK DATA TRANSFERS
+	// total: 5.8 ns lead of any edge of zclk relative to posedge of clk => ACCOUNT FOR THIS WHEN DOING INTER-CLOCK DATA TRANSFERS
 
-	always @(negedge fclk)
+	// Z80 clocking
+	always @(negedge clk)
 	begin
 		if (zpos)
 			zclk_out <= 1'b0;
