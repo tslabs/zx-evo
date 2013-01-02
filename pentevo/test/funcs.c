ae654efa92de224ee193ec723975cd3f879b4a09
at2313 <at2313@gmail.com>
2013-01-02 00:58:44 +0000

    Tests: updated memtest - added TSconf definitions, added some h/w functions

--- pentevo/test/funcs.c
+++ pentevo/test/funcs.c
@@ -1,26 +1,21 @@
 
-#define color(a) cc = (u8)(a)
-#define border(a) output8(0xFE,(a))
-#define page3(a) output(0x13AF,(a))
-#define xy(x, y) cx = (x); cy = (y)
-
+// ASM functions prototypes
 void drawc(u8);
 void ram_fill_p(u8);
 void ram_fill_inc(void);
-u8 ram_check_p(u8);
-u8 ram_check_inc(void);
-void dma_copy(u8);
+u16 ram_check_p(u8);
+u16 ram_check_inc(void);
+
 
 // Screen functions
+#define color(a) cc = (u8)(a)
+#define xy(x, y) cx = (x); cy = (y)
+#define border(a) output8(0xFE,(a))
+
 void cls()
 {
 	memset(vm, 0, 6144);
 	memset(vm + 6144, 0, 768);
-}
-
-void fade()
-{
-	memset(vm + 6144, 1, 768);
 }
 
 void putc(u8 c)
@@ -34,12 +29,6 @@ void putc(u8 c)
 	xy(cx + 6, cy);
 }
 
-int putchar(int c)
-{
-    putc(c);
-    return c;
-}
-
 void msg(u8 *adr)
 {
 	u8 c;
@@ -47,32 +36,23 @@ void msg(u8 *adr)
 		putc(c);
 }
 
-void frame(u8 xx, u8 yy, u8 sx, u8 sy, u8 cf)
+void hex(u8 dig)
 {
-	s8 i, j;
-	xy(xx, yy);
-	
-	color(cf);
-	
-	putc(201);
-	for (i=0; i<sx; i++)
-		putc(205);
-	putc(187);
-	xy(xx, cy + 1);
-	
-	for(j=0; j<sy; j++)
-	{
-		putc(186);
-		for (i=0; i<sx; i++)
-			putc(32);
-		putc(186);
-		xy(xx, cy + 1);
-	}
-	
-	putc(200);
-	for (i=0; i<sx; i++)
-		putc(205);
-	putc(188);
+	putc((dig > 9) ? (dig - 10 + 'A') : (dig + '0'));
+}
+
+void hex8(u8 num)
+{
+	hex((num >> 4) & 0xF);
+	hex(num & 0xF);
+}
+
+void hex16(u16 num)
+{
+	hex((num >> 12) & 0xF);
+	hex((num >> 8) & 0xF);
+	hex((num >> 4) & 0xF);
+	hex(num & 0xF);
 }
 
 // Key functions
@@ -90,9 +70,9 @@ u8 getkey()
 	u8 inp, inpm;
 	s8 key, keyc;
 	u8 i, j;
-	
+
 	while(~input(0xFE) & 0x1F);		// wait for ALL keys to release
-	
+
 	while(1)
 	{
 		row.w = 0xFEFE;
@@ -113,4 +93,59 @@ u8 getkey()
 			row.b.h = (row.b.h << 1) | 1;
 		}
 	}
+}
+
+u8 key_disp()
+{
+	u8 key;
+
+	while(1)
+	{
+		key = getkey();
+		switch(key)
+		{
+			case K_1: { mode = 1; return 1; }
+			case K_2: { mode = 2; return 1; }
+			case K_3: { mode = 0; return 1; }
+			case K_0: { z80_lp ^= 1; return 0; }
+		}
+	}
+}
+
+// H/W functions
+
+	// variables
+	u8 page0;
+	u8 page1;
+	u8 page2;
+	u8 page3;
+	u8 tsconf;
+
+	// inline functions
+#define page0(a)	page0 = (a); output(PAGE0,(a))
+#define page1(a)	page1 = (a); output(PAGE1,(a))
+#define page2(a)	page2 = (a); output(PAGE2,(a))
+#define page3(a)	page3 = (a); output(PAGE3,(a))
+#define tscfg(a)	output(TSCONF,(a))
+#define fmaps(a)	output(FMADDR, ((a)>>12) | FM_EN)
+#define fmaps_off()	output(FMADDR, 0)
+
+#define dma_busy	(input(DSTATUS) & DMA_BSY)
+#define dma_mem()	output(DMACTR, DMA_DEV_MEM);
+#define dma_set(saddr, spage, daddr, dpage, len, num)	\
+					output(DMASAL, saddr & 0xFF);		\
+					output(DMASAH, saddr >> 8);			\
+					output(DMASAX, spage);				\
+					output(DMADAL, daddr & 0xFF);		\
+					output(DMADAH, daddr >> 8);			\
+					output(DMADAX, dpage);				\
+					output(DMALEN, len);				\
+					output(DMANUM, num);
+
+	// functions
+void sfile_null(u16 addr)
+{
+	fmaps(addr);
+	memset((u8*)addr + SFILE, 0, 512);
+	fmaps_off();
 }
--- pentevo/test/main.c
+++ pentevo/test/main.c
@@ -7,50 +7,89 @@
 #include <intrz80.h>
 #include "defs.h"
 #include "funcs.c"
-#include "msg.c"
-#include "menu.c"
 
-int main(void)
+void dma_dummy(u8 pdma)
 {
-	disable_interrupt();
+	dma_set(0, pdma, 0, pdma, 255, 255); dma_mem()
+}
+
+C_task int main(void)
+{
+	u8 f;
 	
+	disable_interrupt();
+	// sfile_null(0x4000);
+	border(0); cls();
+	color(C_HEAD); xy(8,1); msg("Select when to turn TS on:");
+	color(C_NORM); xy(16,3); msg("1. On fill");
+	color(C_NORM); xy(16,4); msg("2. On check");
+	color(C_NORM); xy(16,5); msg("3. Never");
+	color(C_NORM);
+	xy(172,5); msg("0. Z80_LP: ");
+	
+	for(f=0;!f;)
+	{
+		xy(236,5);
+		if (z80_lp) msg("ON "); else msg("OFF");
+		f = key_disp();
+	};
+	
+	color(C_ACHT); xy(0,7); msg("LoAddr");
+	color(C_ACHT); xy(0,16); msg("PgNum");
+    
 	while(1)
 	{
-		cls();
-		color(C_HEAD); xy(16,1);
-		msg("Memory test");
-		color(C_NORM); xy(0,8); msg("Fill pgnum");
-		color(C_NORM); xy(0,16); msg("Fill inc");
-
-		// while(1);
-//		msg_disp();
-//		key_disp();
+		u8 p;
+		u16 tmp;
+		u8 ts = 128 + (z80_lp ? 16 : 0);
 		
-		while(1)
+		for (p=6; p>0; p++)
 		{
-			u8 p;
-			for (p=8; p<255; p++)
-			{
-				border(p&7); 
+			u8 pdma = (p < 160) ? 192 : 128;
 				
-				page3(p);
-				ram_fill_p(p);
-				dma_copy(p);
-				page3(p+1);
-				color(ram_check_p(p) ? C_WARN : C_INFO);
-				cx = (p&31)<<3;
-				cy = ((p&0xE0)>>5) + 8;
-				drawc('#');
-	
-				page3(p);
-				ram_fill_inc();
-				dma_copy(p);
-				page3(p+1);
-				color(ram_check_inc() ? C_WARN : C_INFO);
-				cx = (p&31)<<3;
-				cy = ((p&0xE0)>>5) + 16;
-				drawc('#');
+// --- Increments test ---
+			page3(p);
+			if (mode==1) tscfg(ts);
+			if(!dma_busy) dma_dummy(pdma);
+			ram_fill_inc();
+			tscfg(0);
+			if (mode==2) tscfg(ts);
+			if(!dma_busy) dma_dummy(pdma);
+			color((tmp = ram_check_inc()) ? C_WARN : C_INFO);
+			cx = (p&31)<<3; cy = ((p&0xE0)>>5) + 7; drawc('#');
+			tscfg(0);
+			if (tmp)
+			{
+				u8 *adr = (u8 *)tmp;
+				xy(0,0); color(C_NORM);
+				msg("Page:"); hex8(p);
+				msg("  Addr:"); hex16(tmp);
+				msg("  Written:"); hex8(tmp & 0xFF);
+				msg("  Read:"); hex8(*adr);
 			}
+
+// --- PgNum test ---
+			page3(p);
+			if (mode==1) tscfg(ts);
+			if(!dma_busy) dma_dummy(pdma);
+			ram_fill_p(p);
+			tscfg(0);
+			if (mode==2) tscfg(ts);
+			if(!dma_busy) dma_dummy(pdma);
+			color((tmp = ram_check_p(p)) ? C_WARN : C_INFO);
+			tscfg(0);
+			cx = (p&31)<<3; cy = ((p&0xE0)>>5) + 16; drawc('#');
+			if (tmp)
+			{
+				u8 *adr = (u8 *)tmp;
+				xy(0,0); color(C_NORM);
+				msg("Page:"); hex8(p);
+				msg("  Addr:"); hex16(tmp);
+				msg("  Written:"); hex8(p);
+				msg("  Read:"); hex8(*adr);
+			}
+			
+			ram_fill_p(0);
 		}
 	}
 }
