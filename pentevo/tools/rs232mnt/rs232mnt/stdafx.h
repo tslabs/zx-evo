9cec42c593abc4b4002de9e59c679defd24d5f7a
at2313 <at2313@gmail.com>
2013-05-04 06:58:43 +0000

    Tools: added RS-232 disk mounter

---
+++ pentevo/tools/rs232mnt/rs232mnt/stdafx.h
@@ -0,0 +1,15 @@
+// stdafx.h : include file for standard system include files,
+// or project specific include files that are used frequently, but
+// are changed infrequently
+//
+
+#pragma once
+
+#include <stdio.h>
+#include <windows.h>
+#include <stdlib.h>
+#include <malloc.h>
+#include <memory.h>
+#include <tchar.h>
+#include <sys/stat.h>
+
