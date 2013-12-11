0f27f1e9d3cf570782b7ee323001173041bae2ef
TSL <TSL@localhost>
2013-12-11 21:56:50 +0400

    FPGA: reverted Covox to PWM, added conditional compilation for SDM

--- pentevo/fpga/current/sound/sound.v
+++ pentevo/fpga/current/sound/sound.v
@@ -1,8 +1,10 @@
-`include "../include/tune.v"
 
 // Pentevo project (c) NedoPC 2011
-//
 // integrates sound features: tapeout, beeper and covox
+
+`include "../include/tune.v"
+
+// `define SDM		// uncommented - sigma-delta, commented - PWM
 
 module sound(
 
@@ -19,30 +21,41 @@ module sound(
 	
 );
 
-	reg [8:0] ctr;
-	reg [8:0] val;
-
-	reg mx_beep_n_covox;
-
-	reg beep_bit;
-	reg beep_bit_old;
+	reg [7:0] val;
 
 // port writes
 	always @(posedge clk)
 		if (covox_wr)
-				val[7:0] <= din;
-		else
-		if (beeper_wr)
-				val[8] <= {beeper_mux ? din[3] : din[4]};
+				val <= din;
+		else if (beeper_wr)
+				val <= (beeper_mux ? din[3] : din[4]) ? 8'h7F : 8'h00;
 
-// SD generator
+`ifdef SDM
+// SD modulator
+	reg [7:0] ctr;
+	
 	wire gte = val >= ctr;
 	
 	always @(posedge clk)
 	begin
 		sound_bit <= gte;
-		ctr <= {9{gte}} - val + ctr;
+		ctr <= {8{gte}} - val + ctr;
 	end
 
+`else
+// PWM generator
+	reg [8:0] ctr;
+	
+	wire phase = ctr[8];
+	wire [7:0] saw = ctr[7:0];
+	
+	always @(posedge clk)		// 28 MHz strobes, Fpwm = 54.7 kHz (two semi-periods)
+	begin
+		sound_bit <= ((phase ? saw : ~saw) < val);
+		ctr <= ctr + 9'b1;
+	end
+		
+`endif
+		
 endmodule
 
