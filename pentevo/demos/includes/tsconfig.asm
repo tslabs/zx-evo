47e5bb0a84ac13374bd710ad9c37e86a696e7d75
TSL <TSL@localhost>
2013-12-14 09:10:51 +0400

    Examples: definitions updated

--- pentevo/demos/includes/tsconfig.asm
+++ pentevo/demos/includes/tsconfig.asm
@@ -57,12 +57,22 @@ VID_256X192     equ $00
 VID_320X200     equ $40
 VID_320X240     equ $80
 VID_360X288     equ $C0
+VID_RASTER_BS	equ 6
 
 VID_ZX          equ $00
 VID_16C         equ $01
 VID_256C        equ $02
 VID_TEXT        equ $03
 VID_NOGFX       equ $20
+VID_MODE_BS		equ 0
+
+; PALSEL
+PAL_GPAL_MASK	equ $0F
+PAL_GPAL_BS		equ 0
+PAL_T0PAL_MASK	equ $30
+PAL_T0PAL_BS	equ 4
+PAL_T1PAL_MASK	equ $C0
+PAL_T1PAL_BS	equ 6
 
 ; TSU
 TSU_T0ZEN		equ $04
@@ -75,6 +85,7 @@ TSU_SEN			equ $80
 SYS_ZCLK3_5		equ $00
 SYS_ZCLK7		equ $01
 SYS_ZCLK14		equ $02
+SYS_ZCLK_BS		equ 0
 
 SYS_CACHEEN		equ $04
 
@@ -88,11 +99,13 @@ MEM_LCK512		equ $00
 MEM_LCK128		equ $40
 MEM_LCKAUTO		equ $80
 MEM_LCK1024		equ $C0
+MEM_LCK_BS		equ 6
 
 ; INT
-INT_FRAME		equ $00
-INT_LINE		equ $10
-INT_DMA			equ $20
+INT_SRC_FRAME	equ $00
+INT_SRC_LINE	equ $10
+INT_SRC_DMA		equ $20
+INT_SRC_BS		equ 4
 
 ; DMA
 DMA_WNR         equ $80
@@ -115,6 +128,7 @@ SP_XF			equ $80
 SP_YF			equ $80
 SP_LEAP			equ $40
 SP_ACT			equ $20
+
 SP_SIZE8		equ $00
 SP_SIZE16		equ $02
 SP_SIZE24		equ $04
@@ -123,6 +137,7 @@ SP_SIZE40		equ $08
 SP_SIZE48		equ $0A
 SP_SIZE56		equ $0C
 SP_SIZE64		equ $0E
+SP_SIZE_BS		equ 1
 
 SP_PAL_MASK		equ $F0
 
@@ -130,6 +145,7 @@ SP_XF_W			equ $8000
 SP_YF_W			equ $8000
 SP_LEAP_W		equ $4000
 SP_ACT_W		equ $2000
+
 SP_SIZE8_W		equ $0000
 SP_SIZE16_W		equ $0200
 SP_SIZE24_W		equ $0400
@@ -137,6 +153,7 @@ SP_SIZE32_W		equ $0600
 SP_SIZE48_W		equ $0A00
 SP_SIZE56_W		equ $0C00
 SP_SIZE64_W		equ $0E00
+SP_SIZE_BS_W	equ 9
 
 SP_X_MASK_W		equ $01FF
 SP_Y_MASK_W		equ $01FF
@@ -146,9 +163,12 @@ SP_PAL_MASK_W	equ $F000
 ; TILES
 TL_XF			equ $40
 TL_YF			equ $80
+TL_PAL_MASK		equ $30
+TL_PAL_BS		equ 4
 
 TL_XF_W			equ $4000
 TL_YF_W			equ $8000
 
 TL_TNUM_MASK_W	equ $0FFF
 TL_PAL_MASK_W	equ $3000
+TL_PAL_BS_W		equ 12
