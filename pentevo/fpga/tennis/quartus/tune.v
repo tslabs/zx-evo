f40f477fdd3242faba488bbe79908d444fa5bd26
TS-Labs <TS-Labs@localhost>
2014-07-25 08:56:55 +0300

    FPGA: added Tennis configuration for VDAC (DDp)

---
+++ pentevo/fpga/tennis/quartus/tune.v
@@ -0,0 +1,12 @@
+`timescale 1ns/100ps
+
+`ifdef MODEL_TECH
+`define SIMULATE
+`endif
+
+//`define DRAMMEM_VERBOSE
+//`define FETCH_VERBOSE
+
+// `define FREE_IORQ       // for non-blocked by internal ports !IORQ
+
+// `define IDE_VDAC        // for VideoDAC instead of IDE
