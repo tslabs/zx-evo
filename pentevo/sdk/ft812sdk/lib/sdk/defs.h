4e7d97f86595508bac315f3191894a7a14512965
tslabs <tslabs.info@gmail.com>
2018-03-28 19:44:22 +0300

    SDK: added keys enum

--- pentevo/sdk/ft812sdk/lib/sdk/defs.h
+++ pentevo/sdk/ft812sdk/lib/sdk/defs.h
@@ -17,3 +17,49 @@ typedef void (*TASK)();
 #define max(a,b)  (((a) > (b)) ? (a) : (b))
 #define min(a,b)  (((a) < (b)) ? (a) : (b))
 #define countof(a)  (sizeof(a) / sizeof(a[0]))
+
+// keys
+enum
+{
+  KEY_CS = 0,
+  KEY_Z,
+  KEY_X,
+  KEY_C,
+  KEY_V,
+  KEY_A,
+  KEY_S,
+  KEY_D,
+  KEY_F,
+  KEY_G,
+  KEY_Q,
+  KEY_W,
+  KEY_E,
+  KEY_R,
+  KEY_T,
+  KEY_1,
+  KEY_2,
+  KEY_3,
+  KEY_4,
+  KEY_5,
+  KEY_0,
+  KEY_9,
+  KEY_8,
+  KEY_7,
+  KEY_6,
+  KEY_P,
+  KEY_O,
+  KEY_I,
+  KEY_U,
+  KEY_Y,
+  KEY_EN,
+  KEY_L,
+  KEY_K,
+  KEY_J,
+  KEY_H,
+  KEY_SP,
+  KEY_SS,
+  KEY_M,
+  KEY_N,
+  KEY_B,
+  KEY_NONE
+};
