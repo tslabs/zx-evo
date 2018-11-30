3fa25ddd0bd18182185fb30ac41d74fef8c1a949
tslabs <tslabs.info@gmail.com>
2018-11-30 06:10:41 +0200

    gcWin: added Makefile, fixed build

--- pentevo/sdk/gcWin/build.bat
+++ pentevo/sdk/gcWin/build.bat
@@ -22,11 +22,9 @@ if errorlevel 1 pause & exit
 sdcc.exe --std-c11 -mz80 --no-std-crt0 --opt-code-speed -c src\keyboard.c -o obj\keyboard.rel
 if errorlevel 1 pause & exit
 
-sdcc.exe -o bin\out.hex --std-c11 -mz80 --no-std-crt0 --opt-code-speed -Wl-b_CODE=0x0200 -Wl-b_DATA=0xB000 obj\crt0.rel obj\main.rel obj\gcWin.rel obj\tsio.rel obj\keyboard.rel
+sdcc.exe -o bin\gcwin.hex --std-c11 -mz80 --no-std-crt0 --opt-code-speed -Wl-b_CODE=0x0200 -Wl-b_DATA=0xB000 obj\crt0.rel obj\main.rel obj\gcWin.rel obj\tsio.rel obj\keyboard.rel
 if errorlevel 1 pause & exit
 
-if exist bin\out.hex hex2bin -e bin bin\out.hex
-
-cp bin\out.bin out.bin
+if exist bin\gcwin.hex hex2bin -e bin bin\gcwin.hex
 
 spgbld -b res\spg.ini gcWin.spg
