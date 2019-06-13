d6023279c2ba73c44dd0b3dbcaa1d8e17468d15a
tslabs <tslabs.info@gmail.com>
2019-06-13 21:24:26 +0300

    gcWin: tuned build

--- pentevo/sdk/gcWin/build_with_lib.bat
+++ pentevo/sdk/gcWin/build_with_lib.bat
@@ -7,7 +7,7 @@ if errorlevel 1 pause & exit
 sdcc.exe --std-c11 -mz80 --no-std-crt0 --opt-code-speed -c src\tsio.c -o obj\tsio.rel
 if errorlevel 1 pause & exit
 
-sdcc.exe -o bin\out.hex --std-c11 -mz80 --no-std-crt0 --opt-code-speed -Wl-b_CODE=0x0200 -Wl-b_DATA=0xB000 obj\crt0.rel obj\tsio.rel src\main.c lib\gcwin.lib
+sdcc.exe -o bin\out.hex --std-c11 -mz80 --no-std-crt0 --opt-code-speed -Wl-b_CODE=0x0200 -Wl-b_DATA=0xB000 obj\crt0.rel obj\tsio.rel src\main.c gcwin.lib
 if errorlevel 1 pause & exit
 
 if exist bin\out.hex hex2bin -e bin bin\out.hex
