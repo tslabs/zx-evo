97a40d03b6c86b505fcf32a38f030ad0aca6d006
tslabs <tslabs.info@gmail.com>
2019-06-14 17:25:10 +0300

    SDK: added vars init to start-up code, changed loader to support 32kB code block

--- pentevo/sdk/ft812sdk/lib/sdk/build.bat
+++ pentevo/sdk/ft812sdk/lib/sdk/build.bat
@@ -9,7 +9,7 @@ sdasz80 -o obj/crt0.rel ../lib/sdk/crt0.s
 if errorlevel 1 pause & exit
 
 xcopy /b /y obj\crt0.rel %SDCC%\lib\z80\ >nul
-sdcc -Isrc -I../lib/sdk -I../lib/ft812 -I../lib/tsconf -I../lib/zifi -I../lib/pff -mz80 --std-sdcc11 --opt-code-speed -Wl-b_CODE=0x0040 -Wl-b_DATA=0xB000 src/main.c ../lib/ft812/ft812.lib ../lib/tsconf/ts.lib ../lib/zifi/zifi.lib ../lib/pff/pff.lib ../lib/sdk/sdk.lib -o obj/out.hex
+sdcc -Isrc -I../lib/sdk -I../lib/ft812 -I../lib/tsconf -I../lib/zifi -I../lib/pff -mz80 --std-sdcc11 --opt-code-speed -Wl-b_CODE=0x060 -Wl-b_DATA=0xB000 src/main.c ../lib/ft812/ft812.lib ../lib/tsconf/ts.lib ../lib/zifi/zifi.lib ../lib/pff/pff.lib ../lib/sdk/sdk.lib -o obj/out.hex
 if errorlevel 1 pause & exit
 if exist obj/out.hex hex2bin.exe obj/out.hex >nul
 
