72f93339208ccb05bea3be3d6a2b135444164338
TSL <tslabs.info@gmail.com>
2022-02-10 03:16:06 +0200

    SDK: Updated library build scripts

--- pentevo/sdk/ft812sdk/lib/sdk/makelib.bat
+++ pentevo/sdk/ft812sdk/lib/sdk/makelib.bat
@@ -16,6 +16,6 @@ for /f %%i in (lib.lst) do (
   echo %%i
   sdcc -mz80 --std-sdcc11 --opt-code-speed -c %%i -o %%i.rel
   if errorlevel 1 pause & exit
-  sdcclib ../sdk.lib %%i.rel
+  sdar -rc ../sdk.lib %%i.rel
   if errorlevel 1 pause & exit
 )
