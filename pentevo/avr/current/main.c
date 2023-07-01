bbd81b2e5f0110a646f4e0f1837cfa8335927926
Dmitry <demyanenko.dmitry@gmail.com>
2023-07-02 01:51:27 +0300

    fix load fpga config

--- pentevo/avr/current/main.c
+++ pentevo/avr/current/main.c
@@ -300,8 +300,10 @@ start:
   EIMSK |= (1<<INT4)|(1<<INT5)|(1<<INT6)|(1<<INT7); // enable
 
   kbmap_init();
-  zx_init();
+
   rtc_init(); 
+  zx_init();
+  
   joystick_init();
 
 #ifdef LOGENABLE
